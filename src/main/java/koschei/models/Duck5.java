836f89b30d4b7fbabf2cd33d827e9fa62acdfa52
naP0dXv4t3 <smokewithcheese@gmail.com>
2024-04-09 14:30:10 +0300

    fixed

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -5,14 +5,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
-    private final Egg6 egg6;
+
     @Autowired
-    public Duck5(Egg6 egg6) {
-        this.egg6 = egg6;
-    }
+    private Egg6 egg;
 
     @Override
     public String toString() {
-        return ", в утке яйцо " + egg6.toString();
+        return ", в утке яйцо " + egg.toString();
     }
 }
